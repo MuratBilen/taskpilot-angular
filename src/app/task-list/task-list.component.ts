91a411e69feded52062d26c85d80a6faed8fd7cd
Murat Bilen <hasanmuratbilen@gmail.com>
2025-06-05 13:56:04 +0100

    Added edit task functionality and clear

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import {Task} from "../task/task";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss'
 })
 export class TaskListComponent {
+  //TODO Add linting
+
+  value: string = '';
+
+  isDeleteButtonEnabled: boolean = false;
+
   tasks: Array<Task> = [{
     id: 1,
     title: 'firstTask',
@@ -27,8 +36,31 @@ export class TaskListComponent {
 ]
 
   toggleComplete(selectedTask:Task){
-    this.tasks.find((task)=>
-    task.id=== selectedTask.id
+    this.tasks = this.tasks.map(task =>
+    task.id === selectedTask.id ? {... task, completed: !task.completed} : task
     )
+  }
+
+  addButtonClicked(taskTitle: string) {
+    let idCount = this.tasks.length + 1;
+    console.log('id count is', idCount);
+    this.tasks.push({
+      id: idCount,
+      title: taskTitle,
+      completed: false
+    })
+    console.log('The tasks added', this.tasks);
+  }
+
+  clearButtonClicked(){
+    this.value = '';
+  }
+
+  taskItemClicked(){
+    this.isDeleteButtonEnabled = !this.isDeleteButtonEnabled;
+  }
+
+  deleteButtonClicked(){
+    
   }
 }
